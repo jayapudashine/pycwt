838726c450b423bc2a2512dfa76214c152fff92b
Shrey <shrey.shahi@gmail.com>
2013-08-30 22:33:33 -0400

    fixed some bugs

--- pycwt/pycwt.py
+++ pycwt/pycwt.py
@@ -8,12 +8,12 @@ import pywt
 import matplotlib.pyplot as plt
 
 class CWT:
-	def __init__(self , waveletType):
-		self.wavelet = pywt.Wavelet(waveletType)
-    	[self.phi, self.psi, self.x] = wavelet.wavefun(4)
-    	# Compute the central frequency
-        fft = np.fft.fft(psi)
-    	freqs = np.fft.fftfreq(len(fft) , x[1]-x[0])
+    def __init__(self , waveletType):
+        self.wavelet = pywt.Wavelet(waveletType)
+        [self.phi, self.psi, self.x] = self.wavelet.wavefun(4)
+        # Compute the central frequency
+        fft = np.fft.fft(self.psi)
+        freqs = np.fft.fftfreq(len(fft) , self.x[1] - self.x[0])
         idx = np.nonzero(freqs >= 0)[0] # Only consider positive frequencies
         freqs = freqs[idx]
         fft = fft[idx]
@@ -24,7 +24,7 @@ class CWT:
         return self.centralFreq
 
     def getWavelet(self, scale, dt, time, data):
-        basis = x * self.scale
+        basis = self.x * scale
         basis = basis * dt
         yVals = self.psi/np.sqrt(scale)
         delta = basis[1] - basis[0]
@@ -33,7 +33,7 @@ class CWT:
             signal = data
             modTime = time
             idx = np.nonzero(modTime <= np.max(basis))[0]
-            return np.interp(modTime[idx] , basis, yVals), signal, modTime
+            return np.interp(modTime[idx] , basis, yVals), signal
         else:
             num_pads = np.ceil((max(basis) - max(time))/dt)
             num_pads = max(0,num_pads)
@@ -42,12 +42,15 @@ class CWT:
             modTime = np.arange(0,len(signal))*dt
             return np.interp(modTime , basis, yVals), signal
 
-    def transform(self, data, dt, scales, scaleFactor):
+    def transform(self, data, dt, scales):
         time = np.array(range(len(data)))*dt
         output = np.zeros([len(scales), len(data)])
+        print 'Working on',
         for idx, scale in enumerate(scales):
-            wavelet, signal = getWavelet(scale , dt, time, data)
+            print idx,
+            wavelet, signal = self.getWavelet(scale , dt, time, data)
             output[idx, :] = scipy.signal.convolve(signal, wavelet, mode='same')[:len(data)]
+        print 'Done'
         return output
 
     def scaleToFreq(self , scale, dt = 1):
@@ -56,13 +59,14 @@ class CWT:
     def freqToScale(self, freq, dt = 1):
         return self.centralFreq/(dt * freq)
 
-    def plot(self, data, dt, scales, coefs):
+    def plot(self, data, dt, periods, coefs):
         time = np.arange(0,len(data))*dt
-        X, Y = np.meshgrid(time, scales)
+        X, Y = np.meshgrid(time, periods)
         plt.subplot(2,1,1)
         plt.plot(time , data)
         plt.subplot(2,1,2)
         plt.pcolor(X, Y, coefs**2)
+        plt.yscale('log')
         plt.show()
 
     def savePlot(self, data, dt, scales, coefs, fname):
@@ -72,5 +76,6 @@ class CWT:
         plt.plot(time , data)
         plt.subplot(2,1,2)
         plt.pcolor(X, Y, coefs**2)
+        plt.yscale('log')
         plt.savefig(fname)
         plt.close()
